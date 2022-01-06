5a3788b015669815ddf8d8831300cc31e725baac
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-01-06 22:48:16 +0100

    Saved Suggestions

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-        )
-      ),
+          appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      )),
       home: const RandomWords(),
     );
   }
@@ -40,8 +39,33 @@ class _RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   void _pushSaved() {
-   /*  Navigator.of(context).push( */
-   /*  ); */
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _saved.map(
+        (pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        },
+      );
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList()
+          : <Widget>[];
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Saved Suggestions'),
+        ),
+        body: ListView(children: divided),
+      );
+    },
+    ),
+    );
   }
 
   Widget _buildSuggestions() {
