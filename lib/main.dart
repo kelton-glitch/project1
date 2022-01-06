85e8cdd09c742f7581f7b0ca41c65c9e87a5a81e
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-01-06 20:03:19 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,30 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /* 1 */ (context, i) {
+          if (i.isOdd) return const Divider(); /* 2 */
+
+          final index = i ~/ 2; /* 3 */
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /* 4 */
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
